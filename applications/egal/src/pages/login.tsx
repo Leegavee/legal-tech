824d8474d7970aa4ff9dd2b2260cc948a780ad2a
hiennq <hien124ytbg@gmail.com>
2023-02-15 23:07:05 +0700

    update theme login

--- applications/egal/src/pages/login.tsx
+++ applications/egal/src/pages/login.tsx
@@ -1,71 +1,75 @@
 import Link from 'next/link';
 import React from 'react';
+import { Icons } from '@legavee/icons';
+import Image from 'next/image';
 
 export default function Login() {
-    return (
-        <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
-                   Sign in
-                </h1>
-                <form className="mt-6">
-                    <div className="mb-2">
-                        <label
-                            htmlFor="email"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    <div className="mb-2">
-                        <label
-                            htmlFor="password"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    <div className='flex justify-between'>
-                        <a
-                            href="#"
-                            className="text-xs text-purple-600 hover:underline"
-                        >
-                            Forget Password?
-                        </a>
-                        <Link
-                            href="/api/auth/login"
-                            className="text-xs text-purple-600 hover:underline"
-                        >
-                            Sign in with Google
-                        </Link>
-                    </div>
-                    
-                    <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                            Login
-                        </button>
-                    </div>
-                </form>
-
-                <p className="mt-8 text-xs font-light text-center text-gray-700">
-                    {" "}
-                    Don&apos;t have an account?{" "}
-                    <a
-                        href="#"
-                        className="font-medium text-purple-600 hover:underline"
-                    >
-                        Sign up
-                    </a>
-                </p>
-            </div>
+  return (
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
+          Sign in
+        </h1>
+        <form className="mt-6">
+          <div className="mb-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <a href="#" className="text-xs text-purple-600 hover:underline">
+            Forget Password?
+          </a>
+          <div className="mt-6">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="relative flex items-center justify-center w-full mt-6 border border-t">
+          <div className="absolute px-5 bg-white">Or</div>
         </div>
-    );
+        <div className="flex mt-4 gap-x-2">
+          <Link
+            href="/api/auth/login"
+            className="flex items-center justify-center w-full p-2 border border-gray-600 rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600"
+          >
+            <Image src={Icons.googleIcon} alt="google" />
+          </Link>
+          <Link
+            href="/api/auth/login"
+            className="flex items-center justify-center w-full p-2 border border-gray-600 rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600"
+          >
+            <Image src={Icons.facebookIcon} alt="fb icon" />
+          </Link>
+        </div>
+
+        <p className="mt-8 text-xs font-light text-center text-gray-700">
+          {' '}
+          Don&apos;t have an account?{' '}
+          <a href="#" className="font-medium text-purple-600 hover:underline">
+            Sign up
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
