b4efd55048df004fa9470c3f3a495b5c0e0f6aa8
hiennq <hien124ytbg@gmail.com>
2023-02-15 22:56:08 +0700

    add page login

--- applications/egal/src/pages/index.tsx
+++ applications/egal/src/pages/index.tsx
@@ -1,19 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '@legavee/styles/Home.module.css';
-import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-      <button className='px-6 py-2 rounded bg-slate-400 hover:bg-slate-500 text-slate-100'>
-         <Link className='btn btn-blue' href='/api/auth/login'>
-            login
-        </Link>
-      </button>
+      <h1>Home</h1>
     </>
   );
 }
