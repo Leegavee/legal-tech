e96a7be3f31ed2029c2be4652a7e0a8d450c584f
hiennq <hien124ytbg@gmail.com>
2023-02-16 17:44:22 +0700

    update theme

--- applications/egal/src/pages/chat.tsx
+++ applications/egal/src/pages/chat.tsx
@@ -29,7 +29,7 @@ export default function Chat() {
           <div className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg">
             <div className="h-20 w-20 rounded-full border overflow-hidden">
               <img
-                src="https://avatars3.githubusercontent.com/u/2763884?s=128"
+                src={user?.picture ?? ''}
                 alt="Avatar"
                 className="h-full w-full"
               />
