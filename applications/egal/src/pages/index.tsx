966b6017271ae092b2ae61cc0faab46f50f184e8
Lewis Barclay <lewis.barclay@googlemail.com>
2023-03-31 15:03:09 +0100

    fix: escape apostrophes

--- applications/egal/src/pages/index.tsx
+++ applications/egal/src/pages/index.tsx
@@ -20,15 +20,15 @@ export function Summary() {
         </h1>
         <p className="mt-6 text-xl leading-8">
           Welcome back to your conveyancing portal for your property at 14
-          Glossop Road, CR5 1ED. Here's a summary of your progress so far:
+          Glossop Road, CR5 1ED. Here&apos;s a summary of your progress so far:
         </p>
         <figure className="mt-10 border-l border-indigo-600 pl-9">
           <blockquote className="font-semibold text-gray-900">
             <p>
-              “You've successfully initiated the process by providing us with
-              your instructions. We've obtained the title deeds for the property
-              from the Land Registry, and you've received, signed, and returned
-              the initial paperwork to us. Great job!”
+              “You&apos;ve successfully initiated the process by providing us
+              with your instructions. We&apos;ve obtained the title deeds for
+              the property from the Land Registry, and you&apos;ve received,
+              signed, and returned the initial paperwork to us. Great job!”
             </p>
           </blockquote>
           <figcaption className="mt-6 flex gap-x-4">
@@ -48,8 +48,8 @@ export function Summary() {
         <div className="mt-10 max-w-6xl">
           <p className="mb-4">
             Our team has reviewed the paperwork and sent a draft contract to the
-            buyer's solicitor. They've come back with a few enquiries, and we've
-            forwarded them to you.
+            buyer&apos;s solicitor. They&apos;ve come back with a few enquiries,
+            and we&apos;ve forwarded them to you.
           </p>
           <p className="mb-4">
             Thank you for choosing our services, and we look forward to helping
@@ -79,14 +79,14 @@ export function Summary() {
             <Link href="/progress" className="text-indigo-600 font-bold">
               review and respond
             </Link>{' '}
-            to the enquiries we've sent you. This is an important step to move
-            forward with the conveyancing process.
+            to the enquiries we&apos;ve sent you. This is an important step to
+            move forward with the conveyancing process.
           </p>
           <p className="mt-8 mb-4">
-            Once you provide us with the required information, we'll be able to
-            address the buyer's solicitor's enquiries and continue working
-            towards completing your property sale. If you have any questions or
-            need assistance, please don't hesitate to{' '}
+            Once you provide us with the required information, we&apos;ll be
+            able to address the buyer&apos;s solicitor&apos;s enquiries and
+            continue working towards completing your property sale. If you have
+            any questions or need assistance, please don&apos;t hesitate to{' '}
             <Link href="/chat" className="text-indigo-600 font-bold">
               get in touch
             </Link>
